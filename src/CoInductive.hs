e68d7b7a89da9dd12e2daae2db1c726eed4e5cfb
exclusive-and <exclusiveandgate@gmail.com>
2023-06-24 18:08:06 -0400

    Added some nice docs and sections

--- src/CoInductive.hs
+++ src/CoInductive.hs
@@ -5,46 +5,63 @@
 module CoInductive where
 
 
--- * Mu - Smallest Fixed Point
+-- * Mu - Least Fixed Point
+---------------------------------------------------------------------
 
 -- |
--- @'Mu' f@ is the smallest fixed point of the functor @f@.
+-- @'Mu' f@ is an initial @f@-algebra because it is:
+--
+--  - Initial: for any algebraic type @(x, f x -> x)@, there is
+--    a map @'foldMu' f :: 'Mu' f -> x@.
+--
+--  - Algebraic or @f@-closed: there is a unique morphism
+--    @'inMu' :: f ('Mu' f) -> 'Mu' f@.
+--
+-- By Lambek's lemma, @'Mu' f@ is the least fixed point of @f@.
 --
 data Mu f = Mu (forall a. (f a -> a) -> a)
 
-
--- * Nu - Greatest Fixed Point
-
 -- |
--- @'Nu' f@ is the greatest fixed point of the functor @f@.
+-- The morphism that gives @'Mu' f@ its initiality.
 --
-data Nu f = forall a. Nu (a -> f a) a
-
-
--- * Categorical Properties
-
--- |
--- The unique morphism of an initial @f@-algebra which sends
--- @f a@ to @a@.
--- 
-inMu :: Functor f => f (Mu f) -> Mu f
-inMu x = Mu $ \f -> f (fmap (foldMu f) x)
-
 foldMu :: (f a -> a) -> Mu f -> a
 foldMu f (Mu cata) = cata f
 
+-- |
+--
+--
+inMu :: Functor f => f (Mu f) -> Mu f
+inMu x = Mu $ \f -> f (fmap (foldMu f) x)
+
+
+-- * Nu - Greatest Fixed Point
+---------------------------------------------------------------------
 
 -- |
--- The unique morphism of a terminal @f@-coalgebra which sends
--- @a@ to @f a@.
--- 
+-- @'Nu' f@ is a terminal @f@-coalegbra because it is:
+--
+--  - Final: for any coalgebraic type @(x, x -> f x)@, there is
+--    a map @'Nu' out :: x -> 'Nu' f@.
+--
+--  - Coalgebraic or @f@-consistent: there is a unique morphism
+--    @'outNu' :: 'Nu' f -> f ('Nu' f)@.
+--
+-- By Lambek's lemma, @'Nu' f@ is the greatest fixed point of @f@.
+--
+data Nu f = forall a. Nu (a -> f a) a
+
+-- |
+-- The unique morphism of a terminal @f@-coalgebra.
+--
 outNu :: Functor f => Nu f -> f (Nu f)
 outNu (Nu f x) = fmap (Nu f) (f x)
 
+
+-- * Categorical Properties
+---------------------------------------------------------------------
 
 refold :: Functor f => (f b -> b) -> (a -> f a) -> a -> b
 refold f g = go
     where
     go x = f (fmap go (g x))
-
 
