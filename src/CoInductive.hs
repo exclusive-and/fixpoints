e998780aab9d9d57fa631b55a05d16d9cab8ed02
exclusive-and <exclusiveandgate@gmail.com>
2023-06-26 11:55:00 -0400

    Edited documentation

--- src/CoInductive.hs
+++ src/CoInductive.hs
@@ -9,52 +9,44 @@ module CoInductive where
 ---------------------------------------------------------------------
 
 -- |
--- @'Mu' f@ is an initial @f@-algebra because it is:
---
---  - Initial: for any algebraic type @(x, f x -> x)@, there is
---    a map @'foldMu' f :: 'Mu' f -> x@.
---
---  - Algebraic or @f@-closed: there is a unique morphism
---    @'intoMu' :: f ('Mu' f) -> 'Mu' f@.
---
--- By Lambek's lemma, @'Mu' f@ is the least fixed point of @f@.
+-- @'Mu' f@ is the least fixed point of @f@ by Lambek's lemma.
 --
 data Mu f = Mu (forall a. (f a -> a) -> a)
 
 -- |
--- The morphism that gives @'Mu' f@ its initiality.
---
-foldMu :: (f a -> a) -> Mu f -> a
-foldMu f (Mu cata) = cata f
-
--- |
---
+-- @'Mu' f@ is algebraic or @f@-closed.
 --
 intoMu :: Functor f => f (Mu f) -> Mu f
 intoMu x = Mu $ \f -> f (fmap (foldMu f) x)
+
+-- |
+-- @'Mu' f@ is initial: given an algebraic type @(x, f x -> x)@,
+-- there is a map @'foldMu' f :: 'Mu' f -> x@.
+--
+foldMu :: (f a -> a) -> Mu f -> a
+foldMu f (Mu cata) = cata f
 
 
 -- * Nu - Greatest Fixed Point
 ---------------------------------------------------------------------
 
 -- |
--- @'Nu' f@ is a terminal @f@-coalegbra because it is:
---
---  - Final: for any coalgebraic type @(x, x -> f x)@, there is
---    a map @'Nu' out :: x -> 'Nu' f@.
---
---  - Coalgebraic or @f@-consistent: there is a unique morphism
---    @'outOfNu' :: 'Nu' f -> f ('Nu' f)@.
---
--- By Lambek's lemma, @'Nu' f@ is the greatest fixed point of @f@.
+-- @'Nu' f@ is the greatest fixed point of @f@ by Lambek's lemma.
 --
 data Nu f = forall a. Nu (a -> f a) a
 
 -- |
--- The unique morphism of a terminal @f@-coalgebra.
+-- @'Nu' f@ is coalgebraic of @f@-consistent
 --
 outOfNu :: Functor f => Nu f -> f (Nu f)
 outOfNu (Nu f x) = fmap (Nu f) (f x)
+
+-- |
+-- @'Nu' f@ is terminal: given a coalgebraic type @(x, x -> f x)@,
+-- there is a map @'unfoldNu' f :: x -> 'Nu' f@.
+--
+unfoldNu :: (x -> f x) -> x -> Nu f
+unfoldNu = Nu
 
 
 -- * Categorical Properties
