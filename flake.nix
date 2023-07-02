d9c7f7a8d21c2cce9364b4688aaab78f527948a9
exclusive-and <exclusiveandgate@gmail.com>
2023-07-02 01:29:32 -0400

    Added HLS dev shell to nix flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
   description = "Fixpoints of Functors in Haskell";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url   = "nixpkgs/nixos-unstable";
+    hasklang.url  = "github:haskell/haskell-language-server/783905f211ac63edf982dd1889c671653327e441";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, hasklang }:
     let
       system = "x86_64-linux";
 
@@ -16,6 +17,10 @@
     in
     {
       packages.${system}.default = ghc.callCabal2nix "fixpoints" ./. {};
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ hasklang.packages.${system}.haskell-language-server-961 ];
+      };
     };
 }
 
